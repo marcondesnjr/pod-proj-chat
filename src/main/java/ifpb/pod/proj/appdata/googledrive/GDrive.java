227169e953fc962d4fa25d952141b1ca35ac82a0
MarcondesADS <junior3917@gmail.com>
2016-05-04 18:54:46 -0300

    Primero

---
+++ src/main/java/ifpb/pod/proj/appdata/googledrive/GDrive.java
@@ -0,0 +1,169 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ifpb.pod.proj.appdata.googledrive;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.googleapis.media.MediaHttpDownloader;
+import com.google.api.client.googleapis.media.MediaHttpUploader;
+import com.google.api.client.http.AbstractInputStreamContent;
+import com.google.api.client.http.FileContent;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.InputStreamContent;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.Preconditions;
+import com.google.api.client.util.store.DataStoreFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URL;
+import java.nio.file.Path;
+import java.security.GeneralSecurityException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author José Marcondes do Nascimento Junior
+ */
+public class GDrive {
+
+    private static final String usrFile = "0B9cCXHDSR9etVEFyTUdwQW9PM0E";
+
+    /**
+     * Global instance of the {@link DataStoreFactory}. The best practice is to
+     * make it a single globally shared instance across your application.
+     */
+    private static FileDataStoreFactory dataStoreFactory;
+
+    /**
+     * Application name.
+     */
+    private static final String APPLICATION_NAME
+            = "Drive API Java Quickstart";
+
+    /**
+     * Directory to store user credentials for this application.
+     */
+    private static final java.io.File DATA_STORE_DIR = new java.io.File(
+            System.getProperty("user.home"), ".credentials/pod-proj-chat.json");
+
+    /**
+     * Global instance of the {@link FileDataStoreFactory}.
+     */
+    private static FileDataStoreFactory DATA_STORE_FACTORY;
+
+    /**
+     * Global instance of the JSON factory.
+     */
+    private static final JsonFactory JSON_FACTORY
+            = JacksonFactory.getDefaultInstance();
+
+    /**
+     * Global instance of the HTTP transport.
+     */
+    private static HttpTransport HTTP_TRANSPORT;
+
+    private static final List<String> SCOPES = Arrays.asList(DriveScopes.DRIVE);
+
+    static {
+        try {
+            HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+            DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
+        } catch (Throwable t) {
+            t.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    /**
+     * Authorizes the installed application to access user's protected data.
+     */
+    private Credential authorize() throws Exception {
+        // Load client secrets.
+        InputStream in = this.getClass().getResourceAsStream("/client_secret.json");
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(DATA_STORE_FACTORY)
+                .setAccessType("offline")
+                .build();
+        Credential credential = new AuthorizationCodeInstalledApp(
+                flow, new LocalServerReceiver()).authorize("user");
+        System.out.println(
+                "Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
+        return credential;
+    }
+
+    /**
+     * Build and return an authorized Drive client service.
+     *
+     * @return an authorized Drive client service
+     * @throws IOException
+     */
+    public Drive getDriveService() throws IOException, Exception {
+        Credential credential = authorize();
+        return new Drive.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, credential)
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+    }
+
+//    public static void main(String[] args) throws IOException, Exception {
+//        // Build a new authorized API client service.
+//        Drive service = new GDrive().getDriveService();
+//
+//        // Print the names and IDs for up to 10 files.
+//        FileList result = service.files().list()
+//                .setPageSize(100)
+//                .setFields("nextPageToken, files(id, name)")
+//                .execute();
+//        List<File> files = result.getFiles();
+//        if (files == null || files.size() == 0) {
+//            System.out.println("No files found.");
+//        } else {
+//            System.out.println("Files:");
+//            for (File file : files) {
+//                System.out.printf("%s (%s)\n", file.getName(), file.getId());
+//            }
+//        }
+//    }
+    public static void main(String[] args) throws Exception {
+        File body = new File();
+            body.setTitle("usuarios.xml");
+            body.setMimeType("text/xml");
+        URL url = GDrive.class.getResource("/uploads/usuarios.xml");
+        System.out.println(url);
+        InputStreamContent mediaContent = new InputStreamContent("text/xml", new BufferedInputStream(new FileInputStream(url.getFile())));
+        new GDrive().fileUpload(body, mediaContent);
+    }
+
+    public void fileUpload(File mediaFile, AbstractInputStreamContent aisc) throws FileNotFoundException, Exception {
+        Drive.Files.Update request = getDriveService().files().update(usrFile, mediaFile, aisc);
+        request.execute();
+    }
+
+}
